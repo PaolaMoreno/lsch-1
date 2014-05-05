04afbbaafa64394b0d25aa41c745f9319728ba85
Raul Gracia <raulgracialario@gmail.com>
2014-05-05 02:52:44 +0100

    Adding template and all assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,21 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
+//= require modernizr.custom
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require bootstrap
+//= require classie
+//= require cbpAnimatedHeader.min
+//= require owl.carousel.min
+//= require scrollReveal
+//= require jquery.scrollTo
+//= require jquery.nav
+//= require imagesloaded.pkgd.min
+//= require isotope.min
+//= require jquery.magnific-popup.min
+//= require jqBootstrapValidation
+//= require custom
+
+$(document).ready(function(){
+  isotope();
+  signupOverlay();
+  loginOverlay();
+});
